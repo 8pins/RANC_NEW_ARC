23dcc79f6f4fb2cc1ef3776bbc64f4ffa408beaf
8pins <40314150+8pins@users.noreply.github.com>
2023-06-20 13:33:04 +0700

    Version 1.0

---
+++ rtl_sources/sources_1/ip/fifo_generator_0/fifo_generator_0_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
+-- Date        : Sat Jun 17 15:14:49 2023
+-- Host        : 3ightpins running 64-bit Ubuntu 22.04.2 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/eightpins/RANC/CORE_NEW_ARC/V1.0/V1.0.srcs/sources_1/ip/fifo_generator_0/fifo_generator_0_stub.vhdl
+-- Design      : fifo_generator_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7k325tffg900-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fifo_generator_0 is
+  Port ( 
+    clk : in STD_LOGIC;
+    srst : in STD_LOGIC;
+    din : in STD_LOGIC_VECTOR ( 11 downto 0 );
+    wr_en : in STD_LOGIC;
+    rd_en : in STD_LOGIC;
+    dout : out STD_LOGIC_VECTOR ( 11 downto 0 );
+    full : out STD_LOGIC;
+    wr_ack : out STD_LOGIC;
+    empty : out STD_LOGIC;
+    valid : out STD_LOGIC
+  );
+
+end fifo_generator_0;
+
+architecture stub of fifo_generator_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,srst,din[11:0],wr_en,rd_en,dout[11:0],full,wr_ack,empty,valid";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fifo_generator_v13_2_3,Vivado 2018.3";
+begin
+end;
